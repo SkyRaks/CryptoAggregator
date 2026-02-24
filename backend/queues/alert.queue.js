931d28821327387116e66035a0cd5d2d1eba4f52
SkyRaks <nano.voloshin2016@gmail.com>
2026-02-23 20:42:08 -0500

    some code for alert system

--- backend/queues/alert.queue.js
+++ backend/queues/alert.queue.js
@@ -1,7 +1,20 @@
 import { Queue } from "bullmq";
 import redisConnection from "../config/redis";
+import Alert from "../models/alert.model";
 
 const alertQueue = new Queue("alerts", {
     // Queues will be used by controllers
     connection: redisConnection,
 });
+
+async function addJobs() {
+    const data = await Alert.find({status: "pending"})
+
+    for (const alert of data) {
+        let alertId = data[alert]._id
+
+        await alertQueue.add("check-alert", {
+        alertId: alertId
+    })
+    }  
+}
--- backend/workers/alert.worker.js
+++ backend/workers/alert.worker.js
@@ -1,10 +1,12 @@
 import { Worker, Job } from "bullmq";
 import redisConnection from "../config/redis";
+import User from "../models/user/user.model";
 
 const worker = new Worker("alerts", 
     // checks for changes, sends SMS
-    async job => {
-        console.log(job.data)
+    async (job) => {
+        // Queue will send alertId and then i will procces it here (one at the time)
+        const alertId = job.alertId;
     },
-    { redisConnection },
+    { connection: redisConnection },
 );
