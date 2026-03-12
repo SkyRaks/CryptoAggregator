6601f03d58d465fd7e4d1292ce5bc65a9bbedb1a
SkyRaks <nano.voloshin2016@gmail.com>
2026-03-12 19:34:48 -0400

    redis config change

--- backend/config/redis.js
+++ backend/config/redis.js
@@ -1,10 +1,12 @@
 import IORedis from "ioredis";
 
-const redisConnection = new IORedis({
+const redisConnection = new IORedis(
     // for prod redis, for dev localhost
-    host: "redis",
-    port: 6379,
-    maxRetriesPerRequest: null,
-});
+    process.env.REDIS_URL || {
+        host: "redis",
+        port: 6379,
+        maxRetriesPerRequest: null,
+    },
+);
 
 export default redisConnection;
